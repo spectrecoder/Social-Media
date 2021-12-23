0cff794a2f41e9da970e0caad61ad23ad78bc40c
spectrecoder <pong96111kind@gmail.com>
2021-12-23 15:51:03 +0100

    made routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import './App.css';
+import {useState} from "react";
 import Header from './components/Header';
 import LeftSideBar from './components/LeftSideBar';
+import Login from './components/Login';
 // import LightBox from './components/LightBox';
 import MiddleSection from './components/MiddleSection';
 import RightSideBar from './components/RightSideBar';
 import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+  const [profile, setProfile] = useState('sunny')
+
   return (
     <>
-      {/* <Header/>
-      <LeftSideBar/>
-      <RightSideBar/>
-      <MiddleSection/> */}
-      {/* <LightBox/> */}
-      <SignUp/>
+    {!profile?
+    <Router>
+      <Routes>
+        <Route path="signUp" element={<SignUp/>}/>
+        <Route path="signIn" element={<Login/>}/>
+        <Route path="*" element={<Navigate replace to="/signIn" />}/>
+      </Routes>
+    </Router>
+    :
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Header/>
+            <LeftSideBar/>
+            <RightSideBar/>
+            <MiddleSection/>
+            {/* <LightBox/> */}
+          </>
+        }>
+        </Route>
+        <Route path="*" element={<Navigate replace to="/" />}/>
+      </Routes>
+    </Router>
+    }
     </>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,20 @@
 import React,{useRef} from 'react'
 import { Link } from "react-router-dom";
 
-export default function SignUp() {
+export default function Login() {
     const checkRef = useRef()
 
     return (
-        <section className="signup flex">
+        <section className="login flex">
             <div className="image w-2/3 h-screen flex items-center justify-center relative">
-                <img src="images/signup.svg" alt="sign_up" className="w-full h-full object-contain border-0"/>
+                <img src="images/login.svg" alt="sign_up" className="w-full h-full object-contain border-0"/>
                 <i className="fab fa-phoenix-framework text-8xl text-blue-500 absolute top-8 left-10"></i>
             </div>
             
-            <div className="signup__form w-1/3 h-screen bg-white flex items-center justify-center">
+            <div className="login__form w-1/3 h-screen bg-white flex items-center justify-center">
                 <form className="w-100">
-                    <h3 className="text-3xl font-medium text-gray-500 mb-6">Adventure starts here 🚀</h3>
-                    <p className="text-xl font-normal text-gray-700 mb-8">Make your account management easy and fun!</p>
-
-                    <div className="inputBox mb-6">
-                        <label htmlFor="username" className="label__style">Username</label>
-                        <input autocomplete="off" type="text" placeholder="Username" id="username" className="input__style" required/>
-                    </div>
+                    <h3 className="text-3xl font-medium text-gray-500 mb-6">Welcome to Flames!</h3>
+                    <p className="text-xl font-normal text-gray-700 mb-8">Please sign-in to your account and start the adventure</p>
 
                     <div className="inputBox mb-6">
                         <label htmlFor="email" className="label__style">Email</label>
@@ -36,16 +31,16 @@ export default function SignUp() {
                         <span className="custom__checkbox w-7 h-7 rounded-lg border border-solid border-gray-400 flex items-center justify-center focus:shadow-lg" onClick={()=>checkRef.current.click()}>
                             <i className="fas fa-check text-white text-xl"></i>
                         </span>
-                        <label htmlFor="check" className="label__style">I agree to <span className="text-indigo-500">privacy policy & terms</span></label>
+                        <label htmlFor="check" className="label__style">Remember Me</label>
                     </div>
 
-                    <input type="submit" value="Sign Up" className="w-full py-4 bg-indigo-500 text-white text-2xl text-center rounded-lg mt-6 cursor-pointer hover:shadow-xl"/>
+                    <input type="submit" value="Sign In" className="w-full py-4 bg-indigo-500 text-white text-2xl text-center rounded-lg mt-6 cursor-pointer hover:shadow-xl"/>
 
-                    <p className="label__style text-center mt-8">Already have an account? <Link to="/signIn"><span className="text-indigo-500">Sign in instead</span></Link></p>
+                    <p className="label__style text-center mt-8">New on our platform? <Link to="/signUp"><span className="text-indigo-500">Create an account</span></Link></p>
 
                     <div className="divide text-center label__style mt-9 mb-8 normal-case relative text-2xl orDivider">or</div>
 
-                    <div className="signUp__icons flex gap-4 justify-center">
+                    <div className="login__icons flex gap-4 justify-center">
                         <i className="fab fa-google bg-red-500 smIcon__style"></i>
                         <i className="fab fa-facebook-f smIcon__style bg-blue-600"></i>
                         <i className="fab fa-twitter smIcon__style bg-blue-500"></i>
